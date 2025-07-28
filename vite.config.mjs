e014707c819292ea36b31962cbcfdc17b40108a9
“vinayakgupta45” <vinayakgupta269@gmail.com>
2025-07-29 00:57:10 +0530

    Project complete

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,7 @@ export default defineConfig({
     outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
-  plugins: [tsconfigPaths(), react(), tagger()],
+  plugins: [tsconfigPaths(), react()],
   server: {
     port: "4028",
     host: "0.0.0.0",
